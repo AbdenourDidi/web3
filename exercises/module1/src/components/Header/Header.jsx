eaf58853322417e41a88e9741d9fd6ba7511e9a0
unknown <abdenour.didi@student.vinci.be>
2022-09-20 15:44:24 +0200

    1.1-1.2 : import image

--- exercises/module1/src/components/Header/Header.jsx
+++ exercises/module1/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-
+import logo from './Léonard de Vinci.jpg'
 const Header = (props) =>{
     return(
       <div>
         <h1>{props.course}</h1>
+        <img src={logo} alt="baroni"/>
       </div>
     )
   }
