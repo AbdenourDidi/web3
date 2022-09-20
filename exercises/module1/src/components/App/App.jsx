356213947861f85df67481616fbc4a94d54a1c88
unknown <abdenour.didi@student.vinci.be>
2022-09-20 15:36:40 +0200

    1.1-1.2 : CSS style

--- exercises/module1/src/components/App/App.jsx
+++ exercises/module1/src/components/App/App.jsx
@@ -1,6 +1,6 @@
 
 import React from "react"
-
+import "./App.css"
 import Header from "components/Header/Header"
 import Content from "components/Content/Content"
 import Total from "components/Total/Total"
