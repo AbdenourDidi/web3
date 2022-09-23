6240b5c8a085941266ae8dcb5090e7b89e7b4cd2
unknown <abdenour.didi@student.vinci.be>
2022-09-23 09:37:06 +0200

    part2 Component state, event handlers

---
+++ cours/part2/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState } from "react";
+
+/*const Hello = (props) => {
+  const { name, age } = props;
+  const bornYear = () => new Date().getFullYear() - age;
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} year old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  );
+};*/
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  );
+};
+
+export default App;
