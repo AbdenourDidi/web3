6992aa10c405f2b54acb76ed7e06ef0e676a8a16
unknown <abdenour.didi@student.vinci.be>
2022-10-04 15:46:54 +0200

    module3 : basics

---
+++ exercises/module3/3.1/src/components/App/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const StatisticLine = ({ value, text }) => (
+  <p>
+    {text} {value}
+  </p>
+);
+
+const Statistics = ({ good, bad, neutral }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return <p>No feedback given</p>;
+  }
+  const all = good + neutral + bad;
+  return (
+    <div>
+      <StatisticLine value={good} text="good" />
+      <StatisticLine value={neutral} text="neutral" />
+      <StatisticLine value={bad} text="bad" />
+      <StatisticLine value={all} text="all" />
+      <StatisticLine value={(good - bad) / all} text="average" />
+      <StatisticLine value={(good * 100) / all} text="positive" />
+    </div>
+  );
+};
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
