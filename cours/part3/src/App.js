3bd1c7550cfaba413183ecc3945f155db6ba771d
unknown <abdenour.didi@student.vinci.be>
2022-10-04 14:33:54 +0200

    cours part3 fin

--- cours/part3/src/App.js
+++ cours/part3/src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
 import { useState } from "react";
 
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return <p>the app is used by pressing the buttons</p>;
+  }
+
+  return <p>button press history: {allClicks.join(" ")}</p>;
+};
+
+const Display = (value) => <p>{value.value}</p>;
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0,
-  });
+  const [value, setValue] = useState(10);
 
-  const handleLeftClick = () => {
-    setClicks({ ...clicks, left: clicks.left + 1 });
-  };
-
-  const handleRightClick = () => {
-    setClicks({ ...clicks, right: clicks.right + 1 });
+  const setToValue = (newValue) => {
+    setValue(newValue);
   };
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
