13ebb0cf19b5208aad70d4e411b93242dedeedb1
unknown <abdenour.didi@student.vinci.be>
2022-10-07 08:15:43 +0200

    module3 : loading

--- exercises/module3/3.1/src/components/App/App.js
+++ exercises/module3/3.1/src/components/App/App.js
@@ -13,6 +13,7 @@ const Statistics = ({ good, bad, neutral }) => {
   const all = good + neutral + bad;
   return (
     <div>
+      <h1>statistics</h1>
       <StatisticLine value={good} text="good" />
       <StatisticLine value={neutral} text="neutral" />
       <StatisticLine value={bad} text="bad" />
@@ -23,28 +24,38 @@ const Statistics = ({ good, bad, neutral }) => {
   );
 };
 
-const Loading = ({ loading }) => {
-  if (loading === false) {
-    return <h1>chargement...</h1>;
+const Loading = ({ composant }) => {
+  const [loading, setLoading] = useState(false);
+
+  if (!loading) {
+    setTimeout(() => {
+      setLoading(true);
+    }, 3000);
+    return (
+      <div>
+        <h1>{"Loading...."}</h1>
+      </div>
+    );
+  } else {
+    return composant;
   }
 };
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [loading, setLoading] = useState(false);
 
   return (
     <div>
-      setTimeout(
-      <Loading loading={setLoading(true)} />, 3000)
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Loading
+        composant={<Statistics good={good} neutral={neutral} bad={bad} />}
+      />
     </div>
   );
 };
